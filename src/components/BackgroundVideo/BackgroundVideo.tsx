7ae77dd665a8c300c0fe968b59a6e97b4e3fcd80
jovisjohnson <144215253+jovisjohnson@users.noreply.github.com>
2026-06-27 18:51:45 +0530

    modified

--- src/components/BackgroundVideo/BackgroundVideo.tsx
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -3,9 +3,12 @@ import bgVideo from '../../videos/hero/vr-2.min.mp4';
 
 const BackgroundVideo = () => {
   return (
-    <video className="bg-video" autoPlay loop muted playsInline>
-      <source src={bgVideo} type="video/mp4" />
-    </video>
+    <div className="video-container">
+      <video className="bg-video" autoPlay loop muted playsInline>
+        <source src={bgVideo} type="video/mp4" />
+      </video>
+      <div className="video-overlay"></div>
+    </div>
   );
 };
 
